3f6872e1d10968dfe6f91a4db4cdf31fbce59cc0
Daniel Mauricio B <Mauricio.Bueno@utp.edu.co>
2023-02-27 18:58:40 -0500

    se importan los modelos y se referncian en el archivo admin.py

--- app_inventario/admin.py
+++ app_inventario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from app_inventario.models import *
 
 # Register your models here.
+admin.site.register(articulo_has_empleado)
+admin.site.register(Empleado)
+admin.site.register(Articulo)
+admin.site.register(Categoria)
+admin.site.register(Empresa)
